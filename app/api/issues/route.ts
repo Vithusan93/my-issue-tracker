56a5eb3ec081697abd40b59bddf4855c3e7c0b28
Vithusan Gopalapillai <gvithusan@gmail.com>
2025-03-05 22:30:36 +0100

    prisma client repair

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/prisma/client";
 import { createIssueSchema } from "./validationSchemas";
 
-const prisma = new PrismaClient();
+
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
-'use client';
 
 import React from "react";
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from '@/prisma/client'
 
-const Issuespage = () => {
+const Issuespage = async () => {
+ const issues = await prisma.issue.findMany();
   return <div><Button><Link href= 'issues/new'>New Issue</Link></Button></div>;
 };
 
