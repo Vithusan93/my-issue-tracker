ac9d9cf6dfdeaf17635ec279405b123ded37a6e6
Vithusan Gopalapillai <gvithusan@gmail.com>
2025-03-06 20:32:38 +0100

    add loading skeleton

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,19 +1,15 @@
+import { Table } from "@radix-ui/themes";
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
-import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import IssueActions from "./IssueActions";
 
-const Issuespage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
+const LoadingIssuespage = () => {
+  const issues = [1, 2, 3, 4, 5];
   return (
     <div>
-      <div className="mb-5">
-        <IssueActions />
-      </div>
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -28,18 +24,18 @@ const Issuespage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                {issue.title}
+                <Skeleton />
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createAT.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -49,4 +45,4 @@ const Issuespage = async () => {
   );
 };
 
-export default Issuespage;
+export default LoadingIssuespage;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,58 +1,67 @@
-'use client'
-import { TextField ,Button, Callout, Text, Spinner} from '@radix-ui/themes'
+"use client";
+import { TextField, Button, Callout, Text, Spinner } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
-import {useForm, Controller} from 'react-hook-form'
-import axios from 'axios';
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import {zodResolver} from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/api/issues/validationSchemas';
-import { z } from 'zod'
-import ErrorMessage from '@/app/components/ErrorMessage';
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/api/issues/validationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
-type IssueForm = z.infer<typeof createIssueSchema>
-
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-    const router = useRouter();
-    const {register,control,handleSubmit, formState:{errors} } = useForm<IssueForm>({
-        resolver: zodResolver(createIssueSchema)
-    });
-    const [error, setError] = useState('');
-    const [isSubmitting, setSubmitting] = useState(false)
-    const onSubmit = handleSubmit(async(data) => {
-        try{
-            setSubmitting(true);
-            await axios.post('/api/issues',data);
-            router.push('/issues');
-        } catch (error) {
-            setSubmitting(false);
-            setError('An unexpected error occurred')
-        }
-
-    })
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occurred");
+    }
+  });
 
   return (
-    <div className='max-w-xl'>
-        {error && <Callout.Root color='red'className='mb-5'>
-            <Callout.Text>{error}</Callout.Text>
-            </Callout.Root>}
-    <form className=' space-y-3' onSubmit={onSubmit}>
-        <TextField.Root placeholder='Title' {...register('title')}>
-            <TextField.Slot  />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className=" space-y-3" onSubmit={onSubmit}>
+        <TextField.Root placeholder="Title" {...register("title")}>
+          <TextField.Slot />
         </TextField.Root>
-        <ErrorMessage>{ errors.title?.message}</ErrorMessage>
-        <Controller 
-        name="description"
-        control={control}
-        render={({field}) => <SimpleMDE placeholder='Description' {...field}/> }
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isSubmitting}>Submit New  Issue {isSubmitting && <Spinner/>}</Button>
-    </form>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
